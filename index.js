5b8bef26873fa7b9291438abed3e0f60cde29a55
Umut Bekdemir <umut.bekdemir29@gmail.com>
2025-06-20 20:54:58 +0300

    Connected to MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-import express from "express"
-const app = express()
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
 
-app.listen(8800, ()=>{
-    console.log("Connected to backend.")
-})
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected!");
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend.");
+});
